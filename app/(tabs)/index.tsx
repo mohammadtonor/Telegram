60ec98072b4ecd4b0bdf745e833ff72aa705a339
mohammadtonor <mtonor1368@gmail.com>
2024-09-17 00:34:15 +0330

    prettier and eslint configured

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,18 +1,10 @@
-import {Image, StyleSheet, Platform, View, Text, StatusBar} from 'react-native';
-import {SafeAreaView} from "react-native-safe-area-context";
-import {SplashScreen} from "expo-router";
-import {useEffect} from "react";
-
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function HomeScreen() {
-
   return (
-      <SafeAreaView className="flex-1 items-center justify-center bg-white">
-          <Text>Open up App.js to start working on your app!</Text>
-      </SafeAreaView>
+    <SafeAreaView className="flex-1 items-center justify-center bg-white">
+      <Text>Open up App.js to start working on your app!</Text>
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-
-});
